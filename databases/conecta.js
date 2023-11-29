bdedae6af222ead10520bc6fe65bd535d7fe8b3c
Jennifer <jennifertorchelsen@gmail.com>
2023-11-28 22:30:50 -0300

    ✨ ┇ adicionado mais um dado de referencia a conexao ao banco

--- databases/conecta.js
+++ databases/conecta.js
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME, process.env.DB_HOST, process.env.DB_PASSWORD, {
+  process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   dialect: "mysql",
   host: "localhost",
   port: 3306
